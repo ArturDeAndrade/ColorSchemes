bcacd369ba7bf5e98ab00a9d1d068d7f2e0d539b
ArturDeAndrade <adeandrade.42@gmail.com>
2020-05-29 16:24:26 -0300

    Documented the code. Removed some of the index.html elements adn their respective styles. Improved the index.js file.

--- index.js
+++ index.js
@@ -1,23 +1,36 @@
-const body = document.body
-const buttonBar = document.getElementById('buttonBar')
+// Getting button list.
 const buttonList = document.getElementById('buttonList')
 
-// This function creates a new button(with its own label and id), 
-// adds it to the page and also creates a function to change the page theme(this function works by adding the className parameter to the body element).
+/*
+This function creates a new button(with its own name and id), 
+adds it to the page 
+and also creates a function to change the page theme
+(this function works by adding the className parameter to the body element).
+*/
 function colorButton(name, className, id) {
+    /*
+    Creating a button with the name and id provided 
+    and appending it to the list.
+    */
     this.button = document.createElement('button')
-    this.label = document.createTextNode(name)
-    this.button.appendChild(this.label)
-    buttonList.appendChild(this.button)
-    buttonBar.appendChild(buttonList)
-
+    this.button.innerText = name
     this.button.id = id
-    this.changeColorScheme = function changeColorScheme() {
-        body.classList.remove(...body.classList)
-        body.classList.add(className)
-        console.log("Set to " + name)
+    buttonList.appendChild(this.button)
+    /*
+    Creating a function that removes all classes from the body element 
+    and adds the one from the button that called the function.
+    */
+    this.changeColorScheme = () => {
+        document.body.classList.remove(...document.body.classList)
+        document.body.classList.add(className)
     }
 }
+
+/*
+To create a new button:
+const "variable" = new colorButton("button label", "class-name", "buttonId")
+"variable".button.addEventListener('click', "variable".changeColorScheme)
+*/
 
 const defaultB = new colorButton('Default', 'default', 'default')
 defaultB.button.addEventListener('click', defaultB.changeColorScheme)
@@ -40,5 +53,18 @@ natureB.button.addEventListener('click', natureB.changeColorScheme)
 const luizaB = new colorButton('Luiza Theme', 'luiza-theme', 'luizaTheme')
 luizaB.button.addEventListener('click', luizaB.changeColorScheme)
 
-// const 'button-variable' = new colorButton("button label", "class-name", "button id")
-// 'button-variable'.button.addEventListener('click', 'button-variable'.changeColorScheme)
+/*
+TODO:
+Create a new function(or change the current one)
+that allows the user to add and remove themes;
+This would require the user to enter the following information:
+- The button name, consisting of one word with a capital first letter
+(The new function would then create a class name and id automatically,
+using regular expressions);
+- A primary color;
+- A secondary color;
+- A text color;
+- An outline color;
+(The function would have to somehow create the CSS rules for those,
+and it would be preferable to have multiple ways of getting the colors).
+*/
